d4a011db75a50cdfbbb44a20983f78f4b41e3f98
mosluce <mosluce@gmail.com>
2016-08-17 11:00:26 +0800

    v0.0.1

---
+++ MaterialShadowView/MaterialShadowView.h
@@ -0,0 +1,19 @@
+//
+//  MaterialShadowView.h
+//  MaterialShadowView
+//
+//  Created by 默司 on 2016/8/17.
+//  Copyright © 2016年 默司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialShadowView.
+FOUNDATION_EXPORT double MaterialShadowViewVersionNumber;
+
+//! Project version string for MaterialShadowView.
+FOUNDATION_EXPORT const unsigned char MaterialShadowViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialShadowView/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# MaterialShadowView
+
+## 安裝
+- 方法1： 拖拉 *ShadowView.swift* 和 *MaterialShadowView.swift* 到專案中並勾選 **Copy files if needed** (需要先裝好 SnapKit)
+- 方法2： 可透過 Carthage
